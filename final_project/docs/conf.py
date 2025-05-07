3b9ed11ee66eff14e2e36dae78fa1f91d0cecb12
milacent <you@abc.net>
2025-05-07 19:42:48 +0300

    documentation

---
+++ final_project/docs/conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import sys
+import os
+import django
+
+sys.path.insert(0, os.path.abspath('../'))  # путь до manage.py
+os.environ['DJANGO_SETTINGS_MODULE'] = 'final_project.settings'
+django.setup()
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+]
+
+project = 'project'
+copyright = '2025, Ratmir'
+author = 'Ratmir'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'ru'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
---
+++ final_project/docs/source/final_project_app.management.commands.rst
@@ -0,0 +1,21 @@
+final\_project\_app.management.commands package
+===============================================
+
+Submodules
+----------
+
+final\_project\_app.management.commands.import\_outfits module
+--------------------------------------------------------------
+
+.. automodule:: final_project_app.management.commands.import_outfits
+   :members:
+   :show-inheritance:
+   :undoc-members:
+
+Module contents
+---------------
+
+.. automodule:: final_project_app.management.commands
+   :members:
+   :show-inheritance:
+   :undoc-members:
