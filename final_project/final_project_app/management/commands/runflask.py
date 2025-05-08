196b2a4bb74709bfb00d7c3ed5e6ccbddaca0521
danek <gajfulindaniil93@gmail.com>
2025-05-08 21:48:28 +0300

    need_fixies

--- final_project/final_project_app/management/commands/runflask.py
+++ final_project/final_project_app/management/commands/runflask.py
@@ -5,7 +5,7 @@ class Command(BaseCommand):
     help = 'Запускает Flask сервер'
 
     def handle(self, *args, **kwargs):
-        flask_app_path = 'myflask/app.py'  # Путь до вашего Flask-файла
+        flask_app_path = 'app.py'  # Путь до вашего Flask-файла
         self.stdout.write(self.style.SUCCESS('Запускаем Flask сервер...'))
         try:
             subprocess.run(['python', flask_app_path], check=True)
